123bd5fe92b9f26e7af70d3daeae6ca8d6be0ffe
Bastien Robert <bastienrbt@protonmail.com>
2017-08-07 18:29:25 +0200

    Error service now works with session

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,23 @@
-require('express-session')
-
 module.exports = function(app){
 
+  let session = require('express-session')
+  app.use(session({
+    secret: 'please set a secret key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: false}
+  }))
+
   app.get('/', function(request, response){
+    if (request.session.error) {
+      response.locals.error = request.session.error
+    }
     response.render('pages/index')
+  })
+
+  app.post('/', function(request, response){
+    request.session.error = "New error flash message asked"
+    response.redirect('/')
   })
 
 }
