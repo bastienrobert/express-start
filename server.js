a513d50bf85961d1d15c30bf533bbc15cfcedb32
Bastien Robert <bastienrbt@protonmail.com>
2017-08-07 17:11:05 +0200

    Reorganize app actual dirs - CSS modules enabled

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ let engine = require('ejs-locals')
 let app = express()
 
 app.engine('ejs', engine)
+app.set('views', 'app/views')
 app.set('view engine', 'ejs')
 
-app.use('/assets', express.static('public'))
+app.use('/assets', express.static('app/public'))
 
 app.get('/', (request, response) => {
   response.render('pages/index', {test: "Salut"})
