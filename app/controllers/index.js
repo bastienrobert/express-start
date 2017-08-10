83f1258898f4cb0091c30f81872f91691901f8aa
Bastien Robert <bastienrbt@protonmail.com>
2017-08-10 15:08:16 +0200

    Create controller auto method import let

---
+++ app/controllers/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = function() {
+
+  let fs = require('fs'),
+      path = require('path'),
+      camelize = require(root_path + '/lib/camelize'),
+      basename = path.basename(module.filename),
+      pluralize = require('pluralize'),
+      controllers = {}
+
+  fs
+    .readdirSync(__dirname)
+    .filter(function(file) {
+      return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js')
+    })
+    .forEach(function(file) {
+      var controllerPath = path.join(__dirname, file)
+      var controllerName = camelize(pluralize.singular(
+        path.basename(controllerPath)
+            .split('_')
+            .slice(0, -1)
+            .join(' ')
+      ))
+      controllers[controllerName] = controllerPath
+    })
+    return controllers
+}
